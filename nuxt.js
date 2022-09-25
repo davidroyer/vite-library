0a825cebfa4d242904556fecacaf54c154a9885d
David Royer <droyer@dealertire.com>
2022-09-25 15:57:05 -0400

    fixes import

--- nuxt.js
+++ nuxt.js
@@ -1,10 +1,12 @@
+import { join } from "path";
+
 export default (inlineOptions, nuxt) => {
   // You can do whatever you like here..
   console.log('@HERE2 - FROM NUXT FILE');
 
   nuxt.hook('components:dirs', dirs => {
     dirs.push({
-      path: path.join(__dirname, 'src/components'),
+      path: join(__dirname, 'src/components'),
       prefix: '',
       extensions: ['vue']
     });
