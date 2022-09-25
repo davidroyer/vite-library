16d5cec31eeda5221c5eb5a06cdfa91f55563f81
David Royer <droyer@dealertire.com>
2022-09-25 15:56:05 -0400

    fixes path error

--- nuxt.js
+++ nuxt.js
@@ -4,7 +4,7 @@ export default (inlineOptions, nuxt) => {
 
   nuxt.hook('components:dirs', dirs => {
     dirs.push({
-      path: join(__dirname, 'src/components'),
+      path: path.join(__dirname, 'src/components'),
       prefix: '',
       extensions: ['vue']
     });
