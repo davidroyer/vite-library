a781e702f681d849c72945965e625a6c442f4960
David Royer <droyer@dealertire.com>
2022-09-25 15:59:35 -0400

    chore: cleanup

--- nuxt.js
+++ nuxt.js
@@ -1,9 +1,6 @@
-import { join } from "path";
+import { join } from 'path';
 
 export default (inlineOptions, nuxt) => {
-  // You can do whatever you like here..
-  console.log('@HERE2 - FROM NUXT FILE');
-
   nuxt.hook('components:dirs', dirs => {
     dirs.push({
       path: join(__dirname, 'src/components'),
