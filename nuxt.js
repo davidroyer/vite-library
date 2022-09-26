34fc23d4b5bdcbe3ec9b010162e798d173caee18
David Royer <droyer@dealertire.com>
2022-09-26 12:32:44 -0400

    chore: testing

--- nuxt.js
+++ nuxt.js
@@ -1,11 +1,26 @@
 import { join } from 'path';
+import { defineNuxtModule, addComponentsDir, installModule } from '@nuxt/kit';
 
-export default (inlineOptions, nuxt) => {
-  nuxt.hook('components:dirs', dirs => {
-    dirs.push({
+export default defineNuxtModule({
+  async setup(moduleOptions, nuxt) {
+    await addComponentsDir({
       path: join(__dirname, 'src/components'),
       prefix: '',
       extensions: ['vue']
     });
-  });
-};
+
+    await installModule('@nuxtjs/tailwindcss')
+  }
+});
+
+// export default (inlineOptions, nuxt) => {
+//   // await installModule('@nuxt/postcss8')
+
+//   nuxt.hook('components:dirs', dirs => {
+//     dirs.push({
+//       path: join(__dirname, 'src/components'),
+//       prefix: '',
+//       extensions: ['vue']
+//     });
+//   });
+// };
